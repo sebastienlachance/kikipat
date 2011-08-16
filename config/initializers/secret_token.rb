75907f982f63da62a23fedb9b991674345e38592
Sebastien Lachance <sebastien.lachance@gmail.com>
2011-08-16 19:41:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kikipat::Application.config.secret_token = '392da3eebaec8b8e9c521f29e7c49d4dc736546cfe1e85d08ed34a3b87ad70976b18578245008e21613540518fa2985268b88928bfe08740f46a99a8066c5cae'
